59697814a9cfd2ec5e2c700be95c9111aa1cfe4d
henrik <henha806@student.liu.se>
2025-03-07 07:53:40 +0100

    Added cloud animations

--- components/Sky.vue
+++ components/Sky.vue
@@ -1,14 +1,51 @@
 <template>
     <div class="sky">
+        <div class="clouds"></div>
     </div>
 </template>
 
+<script>
+export default {
+    mounted() {
+        this.startAnimation();
+    },
+    methods: {
+        startAnimation() {
+            const cloudsElement = this.$el.querySelector('.clouds');
+            cloudsElement.style.animation = 'scrollBackground 60s linear infinite';
+        }
+    }
+};
+</script>
+
 <style>
 .sky {
-    position:absolute;
-    z-index:-4;
-    width:100%;
+    position: absolute;
+    z-index: -4;
+    width: 100%;
     height: 100dvh;
-    background-image: linear-gradient(#759bf8,#a3eaf1);
+    background-image: linear-gradient(#759bf8, #a3eaf1);
+    overflow: hidden;
+}
+
+.clouds {
+    position: absolute;
+    top: 0;
+    width: 100%;
+    height: 50%;
+    background-image: url('@/public/clouds.svg');
+    background-repeat: repeat-x;
+    background-size: cover;
+    overflow: hidden;
+    animation: scrollBackground 60s linear infinite;
+}
+
+@keyframes scrollBackground {
+    0% {
+        background-position: 0 0;
+    }
+    100% {
+        background-position: -2000px 0;
+    }
 }
 </style>
